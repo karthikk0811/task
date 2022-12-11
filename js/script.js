79b3523330685b3c833c52c5305141392c65f4cb
karthik <120299758+karthikk0811@users.noreply.github.com>
2022-12-11 13:30:44 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.logos').slick({
+        dots: false,
+        autoplay: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+      });
+})
